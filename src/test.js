2c31a1c511c08ca9ebb3170daac1fc5555a5333f
Pritika Sharma <pritika.sharma@celigo.com>
2024-04-04 15:24:30 +0530

    test21

---
+++ src/test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 My App</p>
+      <p> Hello I like coding.</p>
+      <p> Hello I like coding.</p>
+      <p> Hello I like coding.</p>
+      <p> Hello I like coding.</p>
+
+      <p> hellossss</p>
+      <h2>hgjh</h2>
+      <h1>ggdfdf</h1>
+      <h1>
+        yo
+      </h1>
+      <h2>
+        fdfds
+      </h2>
+    </footer>
+  );
+}
+
+export default footer;
