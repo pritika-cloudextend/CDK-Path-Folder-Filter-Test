9cdc38206a9964e8fd7526737f02ec4b53ed7fe9
Pritika Sharma <pritika.sharma@celigo.com>
2024-04-04 12:06:04 +0530

    test12

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,8 @@ function about() {
       <h2>About Page</h2>
       <p>This is the About Page of My App.</p>
       <p> This ia the page about.</p>
+      <p> This ia the page about.</p>
+
       <p> hiiiiiiii</p>
       <p> hiiiiiiii</p>
       <p> hiiiiiiii</p>
