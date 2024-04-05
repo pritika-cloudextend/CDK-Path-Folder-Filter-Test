7d44f5c2298dab2fa980662ae28e374e944515ab
Pritika Sharma <pritika.sharma@celigo.com>
2024-04-05 13:04:18 +0530

    test35

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,7 @@ function footer() {
       <p> Hello I like coding.</p>
       <p> Hello I like coding.</p>
       <p> Hello I like coding.</p>
+      <p> Hello I like coding.</p>
 
       <p> hellossss</p>
       <p> hellossss</p>
