f727ff18b49b04bf9b74e7176980c183f97bf4ea
Pritika Sharma <pritika.sharma@celigo.com>
2024-04-05 11:05:13 +0530

    test30

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,8 @@ function home() {
       <p>Welcome to the Home Page of My App!</p>
       <p>Welcome to the Home Page of My App!</p>
       <p>Welcome to the Home Page of My App!</p>
+      <p>Welcome to the Home Page of My App!</p>
+
 
 
 
