2d84fae1466894bd253c8f79b5165ed6baeca3c4
Pritika Sharma <pritika.sharma@celigo.com>
2024-04-10 12:23:19 +0530

    test58

--- src/test1/test2/test3.js
+++ src/test1/test2/test3.js
@@ -5,5 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to My App!");
     alert("Welcome to My App!");
     alert("Welcome to My App!");
+    alert("Welcome to My App!");
+
 
 });
